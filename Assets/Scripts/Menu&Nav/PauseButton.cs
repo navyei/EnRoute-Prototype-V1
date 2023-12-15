812c783f591bd6a6ced464b52ff018c95127534c
fenmorre <fenmorre@gmail.com>
2023-12-15 19:29:26 +0700

    Update PauseButton.cs

--- Assets/Scripts/Menu&Nav/PauseButton.cs
+++ Assets/Scripts/Menu&Nav/PauseButton.cs
@@ -10,11 +10,13 @@ public class PauseButton : MonoBehaviour
     public void Pause()
     {
         pauseMenu.SetActive(true);
+        Time.timeScale = 0;
     }
 
     public void Resume()
     {
         pauseMenu.SetActive(false);
+        Time.timeScale = 1;
     }
 }
 
