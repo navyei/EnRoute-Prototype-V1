63da7a51319f933a34c3959997ab6360f9a4136f
DankPam <98955325+dangchodien@users.noreply.github.com>
2023-12-19 12:41:33 +0700

    Minigame Update

--- Assets/Scripts/Minigame 1/ObjectSpawner.cs
+++ Assets/Scripts/Minigame 1/ObjectSpawner.cs
@@ -8,14 +8,20 @@ public class ObjectSpawner : MonoBehaviour
     public GameObject objectPrefab;
     public float initialSpawnArea = 10.0f;
     public float spawnInterval = 2.0f;
-
+    public int maxObstacles = 10;
+    private int spawnedObstacleCount = 0;
     private float timer = 0.0f;
 
     private void Update()
     {
+        if (spawnedObstacleCount >= maxObstacles)
+        {
+            return; // Stop spawning if the limit is reached
+        }
+
         // Update the timer
         timer += Time.deltaTime;
-
+        
         // Check if it's time to spawn a new object
         if (timer >= spawnInterval)
         {
@@ -25,9 +31,7 @@ public class ObjectSpawner : MonoBehaviour
             // Increase spawn area by 2 units
             spawnInterval += 2.0f;
         }
-
-        // Check for collision with "M1" during each update
-        CheckForM1Collision();
+        
     }
 
     private void SpawnObject()
@@ -36,7 +40,7 @@ public class ObjectSpawner : MonoBehaviour
         Vector3 spawnPosition = new Vector3(
             Random.Range(-initialSpawnArea, initialSpawnArea),
             10.0f, // Spawn at the top of the spawn area
-            Random.Range(-initialSpawnArea, initialSpawnArea)
+            13f
         );
 
         // Instantiate the object prefab at the random position
@@ -52,26 +56,37 @@ public class ObjectSpawner : MonoBehaviour
 
         // Attach a script to handle collisions
         newObj.AddComponent<DestroyOnCollision>();
+        spawnedObstacleCount++;
     }
 
-    private void CheckForM1Collision()
-    {
+    //private void CheckForM1Collision()
+   // {
         // Check if the player collides with an object tagged as "M1"
-        Collider[] colliders = Physics.OverlapSphere(transform.position, 1.0f); // You might need to adjust the radius
-        foreach (Collider collider in colliders)
-        {
-            if (collider.CompareTag("M1"))
-            {
-                // Player collided with "M1," handle loss condition and change scene
-                HandleLoss();
-            }
-        }
-    }
+       // Collider[] colliders = Physics.OverlapSphere(transform.position, 1.0f); // You might need to adjust the radius
+       // foreach (Collider collider in colliders)
+       // {
+       //     if (collider.CompareTag("M1"))
+       //     {
+        //        // Player collided with "M1," handle loss condition and change scene
+       //         HandleLoss();
+        //        Destroy(this.gameObject);
+      //      }
+       // }
+//   }
 
-    private void HandleLoss()
-    {
+    //private void HandleLoss()
+   // {
+        //StopAllCoroutines();
         // Add logic to handle loss (e.g., show game over screen, reset game, etc.)
         // For now, we'll just reload the navigation scene
-        SceneManager.LoadScene("Navigation");
-    }
+       // gameManager.mini1Win = false;
+        //SceneManager.LoadScene("Navigation");
+   // }
+
+   // IEnumerator Win()
+    //{
+        //yield return new WaitForSecondsRealtime(20f);
+        //gameManager.mini1Win = true;
+        //SceneManager.LoadScene("Nagivation");
+   // }
 }
