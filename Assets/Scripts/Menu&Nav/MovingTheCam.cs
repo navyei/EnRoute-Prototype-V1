a17c13aa3c65a1841e2fce3076f56fc3c12bb950
CirnoDayo <nghoanduy123@gmail.com>
2023-12-22 00:35:52 +0700

    I COOKED

--- Assets/Scripts/Menu&Nav/MovingTheCam.cs
+++ Assets/Scripts/Menu&Nav/MovingTheCam.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 
 public class MovingTheCam : MonoBehaviour
 {
+    public bool TurnEnabled;
+    public bool PanEnabled;
     public float CamSpeed;
 
     public Camera Camera;
@@ -24,28 +26,34 @@ public class MovingTheCam : MonoBehaviour
     {
         MouseX = Input.GetAxis("Mouse X");
         MouseY = Input.GetAxis("Mouse Y");
+
         Vector3 CamFwrd = Camera.main.transform.forward; CamFwrd.y = 0f; CamFwrd.Normalize();
         Vector3 CamRght = Camera.main.transform.right; CamRght.y = 0f; CamRght.Normalize();
         Vector3 Movement = CamFwrd * MouseY + CamRght * MouseX;
 
-        if (Input.GetKey(KeyCode.Mouse1))
+        if (Input.GetKey(KeyCode.Mouse1) && TurnEnabled)
         {
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
             OrbTrans.m_XAxis.m_InputAxisName = "Mouse X";
         }
-        else if (Input.GetKey(KeyCode.Mouse2))
+        else if (Input.GetKey(KeyCode.Mouse2) && PanEnabled)
         {
             transform.Translate(Movement * CamSpeed * Time.deltaTime * -1, Space.World);
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
         }
+        else if (OrbTrans != null) 
+        {
+            OrbTrans.m_XAxis.m_InputAxisName = "";
+            OrbTrans.m_XAxis.m_InputAxisValue = 0f;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
         else
         {
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
-            OrbTrans.m_XAxis.m_InputAxisName = "";
-            OrbTrans.m_XAxis.m_InputAxisValue = 0f;
         }
     }
 }
